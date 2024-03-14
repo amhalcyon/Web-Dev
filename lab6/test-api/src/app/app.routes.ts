ca83a81eb570bf487ce5e1fc184e9261ddb1f772
amhalcyon <adinamedelbai@gmail.com>
2024-03-14 11:21:58 +0600

    temp upd

--- lab6/test-api/src/app/app.routes.ts
+++ lab6/test-api/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import {AboutComponent} from "./about/about.component";
 import { AlbumsComponent } from './albums/albums.component';
-import { NotFoundComponent } from "./not-found/not-found.component";
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
 import { AlbumPhotosComponent } from './album-photos/album-photos.component';
 
@@ -12,6 +11,5 @@ export const routes: Routes = [
     {path: 'about', component: AboutComponent, title: 'About'},
     {path: 'albums', component: AlbumsComponent, title: 'Albums'},
     {path: 'albums/:id', component: AlbumDetailComponent, title: 'AlbumDetail'},
-    {path: 'albums/:id/photos', component: AlbumPhotosComponent, title: 'AlbumPhotos'},
-    {path: '**', component: NotFoundComponent, title: '404 Not Found'}
+    {path: 'albums/:id/photos', component: AlbumPhotosComponent, title: 'AlbumPhotos'}
 ];
